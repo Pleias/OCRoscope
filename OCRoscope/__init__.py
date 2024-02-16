ab0abff0732b86372189b5d2d025f8c654696aa1
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-17 00:44:18 +0100

    Update __init__.py

--- OCRoscope/__init__.py
+++ OCRoscope/__init__.py
@@ -1,3 +1,3 @@
 # __init__.py
 from .ocr_evaluation import ocr_evaluation
-from .ocr_functions import detect_languages, letter_ratio, split_text, split_text_sampling
+from .ocr_function import detect_languages, letter_ratio, split_text, split_text_sampling
