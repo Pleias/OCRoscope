43b8f9246a9686becf48728c5559db996c49903c
Pleias <159772114+Pleias@users.noreply.github.com>
2024-02-17 00:29:51 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OCRoscope',
+    version='0.1.0',
+    description='A Python package for OCR evaluation metrics',
+    author='Pierre-Carl Langlais',
+    url='https://github.com/Pleias/marginalia',
+    packages=find_packages("pycld2"),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+)
